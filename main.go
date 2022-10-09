229d53dd2b17b79d64b8456b2482d141a14481e5
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2022-10-09 17:39:52 +0300

    Learn how to use arrays, slices, loops, if statements, switch statement

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 // in golang projects everything get into packages
 package main
 // imports to provide extenrnal functionality
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // starting point of every application is main function
 func main() {
@@ -13,32 +16,82 @@ func main() {
 	// simple const
 	const cinemaTickets = 50
 	var remainingTickets uint = 50
+	// arrays (always fixed size)
+	// var bookings [50]string
+	
+	// slice is below
+	var bookings []string
 
 	// to print types of variables type %T
-	fmt.Printf("cinemaName type is %T\n", cinemaName)
+	// fmt.Printf("cinemaName type is %T\n", cinemaName)
 	
 	// fmt.Printf is for formating prints with variables
 	fmt.Printf("Welcome to the %v booking application\n", cinemaName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", cinemaTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets int
+	// infinite for loop to book tickets 
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+	
+		// ask users for their names by using pointer (address of var where it located)
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
+	
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+	
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email)
+	
+		fmt.Println("Amount of tickets to buy: ")
+		fmt.Scan(&userTickets)
 
-	// ask users for their names by using pointer (address of var where it located)
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+		
+		if !isValidName || !isValidEmail || !isValidTicketNumber  {
+			if !isValidName {
+				fmt.Println("Your name / last name lenght must be at least 2 characters")
+			}
 
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+			if !isValidEmail {
+				fmt.Println("Your email address must contain '@' symbol")
+			}
 
-	fmt.Println("Enter your email: ")
-	fmt.Scan(&email)
+			if !isValidTicketNumber {
+				fmt.Println("Your ticket number must be less or equal to remaining tickets number")
+			}
+			continue
+		}
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName + " " + lastName)
+	
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, cinemaName)
+	
+		firstNames := []string{}
 
-	fmt.Println("Amount of tickets to buy: ")
-	fmt.Scan(&userTickets)
+		// for loop always need 2 values (index and element)
+		// we do not use index so it turns into underscore '_'
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("All bokings: %v\n", firstNames)
+
+		// if statement
+		if remainingTickets == 0 {
+			// end program
+			fmt.Println("Our cinema is booked out. Come back next week")
+			break
+		}
+	}
+
+	
 }
